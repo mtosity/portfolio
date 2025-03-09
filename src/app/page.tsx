d007a0049978648fdb93ee08cf4a02b8ab0e3885
mtosity <mtosity@gmail.com>
2025-03-09 08:55:05 -0500

    remove block vn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,8 @@ import { Projects } from "@/components/template/home/projects/Projects";
 // import { Heading } from "@/components/template/nav/Heading";
 import styles from "../components/template/home/home.module.scss";
 import { Contact } from "@/components/template/home/contact/Contact";
-import { headers } from "next/headers";
 
 export default function Home() {
-  // Get country from headers
-  const headersList = headers();
-  const country = headersList.get("x-vercel-ip-country") || "";
-
-  // Return 404 for Vietnam visitors
-  if (country.toLowerCase() === "vn") {
-    return (
-      <div className="flex flex-col items-center px-4 py-40 ">
-        <div className={styles.home}>
-          <h1 className="text-4xl font-bold text-center">
-            404 - Heart Not Found
-          </h1>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="relative">
       <div className="sticky top-4 bg-transparent z-10">
